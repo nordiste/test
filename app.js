c9af9599593a441dbca6121bee89c3c04add3dbf
nordiste <marc.druelle@gmail.com>
2019-01-09 22:02:06 +0100

    modifié :         app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
-  res.end('Salut tout le monde !');
+  res.end('Salut tout le monde  2!');
+console.log("entrre");
 });
 server.listen(8080);
